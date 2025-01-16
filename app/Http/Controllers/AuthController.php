cfdbdb10c72989088a8e3645706db401c2ff2406
qaysibrahim01 <147976955+qaysibrahim01@users.noreply.github.com>
2025-01-17 00:01:08 +0800

    Add files via upload

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    // Show the login form
+     function login()
+    {
+        return view('auth.login');
+    }
+
+     function loginPost(Request $request)
+{
+    $request->validate([
+        "email" => "required",
+        "password" => "required",
+      ]);
+
+    $credentials = $request->only("email","password");
+
+   if(Auth::attempt($credentials)){
+     return redirect()->intended(route("home"));
+   }
+   return redirect(route("login"))->with("error","Login failed");
+}
+
+
+
+    function register()
+    {
+        return view ("auth.register");
+    }
+    
+    function registerPost(Request $request){
+      $request->validate([
+        "fullname" => "required",
+        "email" => "required",
+        "password" => "required",
+      ]);
+      
+      $user =new User();
+      $user->name =$request->fullname;
+      $user->email =$request->email;
+      $user->password= Hash::make($request->password);
+      if ($user->save()){
+         return redirect(route("login"))->with("success","User created successfully");
+      }
+
+      return redirect(route("register"))
+      ->with("error","Failed to create account");
+    }
+
+}
